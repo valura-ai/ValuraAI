2b1260def2e11ad1a52455ac3586a70954642be6
Sudarshan <105160284+Sudarshan50@users.noreply.github.com>
2025-05-25 22:30:00 +0530

    feat: add environment variables configuration to next.config.js..

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        WORKOS_API_KEY: process.env.WORKOS_API_KEY,
+        WORKOS_APP_SECRET: process.env.WORKOS_APP_SECRET,
+        WORKOS_COOKIE_PASSWORD: process.env.WORKOS_COOKIE_PASSWORD,
+        TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+        TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+        TWILIO_VERIFY_SERVICE_SID: process.env.TWILIO_VERIFY_SERVICE_SID,
+        DATABASE_URL: process.env.DATABASE_URL,
+    }
+};
 
 module.exports = nextConfig;
