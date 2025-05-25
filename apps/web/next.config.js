4f5fed8ae8781696d161f5bd740bf5a4a1bba8d2
Sudarshan <105160284+Sudarshan50@users.noreply.github.com>
2025-05-25 22:36:30 +0530

    Revert "feat: add environment variables configuration to next.config.js.."
    This reverts commit 2b1260def2e11ad1a52455ac3586a70954642be6.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env:{
-        WORKOS_API_KEY: process.env.WORKOS_API_KEY,
-        WORKOS_APP_SECRET: process.env.WORKOS_APP_SECRET,
-        WORKOS_COOKIE_PASSWORD: process.env.WORKOS_COOKIE_PASSWORD,
-        TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-        TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-        TWILIO_VERIFY_SERVICE_SID: process.env.TWILIO_VERIFY_SERVICE_SID,
-        DATABASE_URL: process.env.DATABASE_URL,
-    }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
